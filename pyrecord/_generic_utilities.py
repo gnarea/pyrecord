4f40b701c0c2636011d2c8da607ae186b4fea349
Gustavo Narea <me@gustavonarea.net>
2013-04-02 22:41:36 +0100

    De-duped counting of duplicated items in get_duplicated_iterable_items()
    And added tests for this function

--- pyrecord/_generic_utilities.py
+++ pyrecord/_generic_utilities.py
@@ -4,11 +4,15 @@ __all__ = [
 
 
 def get_duplicated_iterable_items(iterable):
-    iterable_with_unique_items = []
-    iterable_with_duplicated_items = []
+    unique_items = []
+    duplicated_items = []
     for item in iterable:
-        if item in iterable_with_unique_items:
-            iterable_with_duplicated_items.append(item)
+        if item in duplicated_items:
+            continue
+        
+        if item in unique_items:
+            duplicated_items.append(item)
         else:
-            iterable_with_unique_items.append(item)
-    return iterable_with_duplicated_items
+            unique_items.append(item)
+    
+    return duplicated_items
---
+++ tests/test_generic_utilities.py
@@ -0,0 +1,43 @@
+from unittest.case import SkipTest
+
+from nose.tools import eq_
+from nose.tools import ok_
+
+from pyrecord._generic_utilities import get_duplicated_iterable_items
+
+
+__all__ = [
+    "TestDuplicatedIterableItems",
+    ]
+
+
+class TestDuplicatedIterableItems(object):
+    
+    def test_no_items(self):
+        original_iterable = []
+        duplicated_items = get_duplicated_iterable_items(original_iterable)
+        eq_(0, len(duplicated_items))
+    
+    def test_unique_items(self):
+        original_iterable = [1, 2, 3]
+        duplicated_items = get_duplicated_iterable_items(original_iterable)
+        eq_(0, len(duplicated_items))
+    
+    def test_one_duplicate(self):
+        original_iterable = [1, 2, 2]
+        duplicated_items = get_duplicated_iterable_items(original_iterable)
+        eq_(1, len(duplicated_items))
+        ok_(2 in duplicated_items)
+    
+    def test_two_duplicates_of_same_item(self):
+        original_iterable = [1, 2, 2, 2]
+        duplicated_items = get_duplicated_iterable_items(original_iterable)
+        eq_(1, len(duplicated_items))
+        ok_(2 in duplicated_items)
+    
+    def test_duplicates_of_various_items(self):
+        original_iterable = [1, 1, 2, 2]
+        duplicated_items = get_duplicated_iterable_items(original_iterable)
+        eq_(2, len(duplicated_items))
+        ok_(1 in duplicated_items)
+        ok_(2 in duplicated_items)
