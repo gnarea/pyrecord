3f4df4692c34e360b8d145f849f46ea7cfe8731a
Gustavo Narea <me@gustavonarea.net>
2013-04-08 22:43:10 +0100

    Removed duplication of fixture.

--- tests/test_record.py
+++ tests/test_record.py
@@ -7,22 +7,24 @@ from pyrecord import Record
 from pyrecord import RecordInitializationError
 
 
+Point = Record.create_type("Point", "coordinate_x", "coordinate_y")
+Circle = Point.create_type("Circle", "radius")
+
+
 class TestInitialization(object):
     
     def test_initialization(self):
-        Point = Record.create_type("Point", "coordinate_x")
-        my_point = Point(1)
+        my_point = Point(1, 3)
         ok_(isinstance(my_point, Point))
         eq_(my_point.coordinate_x, 1)
+        eq_(my_point.coordinate_y, 3)
     
     def test_initialization_by_name(self):
-        Point = Record.create_type("Point", "coordinate_x")
-        my_point = Point(coordinate_x=1)
+        my_point = Point(coordinate_x=1, coordinate_y=3)
         eq_(my_point.coordinate_x, 1)
+        eq_(my_point.coordinate_y, 3)
     
     def test_subtype(self):
-        Point = Record.create_type("Point", "coordinate_x", "coordinate_y")
-        Circle = Point.create_type("Circle", "radius")
         my_circle = Circle(1, 3, 5)
         eq_(my_circle.coordinate_x, 1)
         eq_(my_circle.coordinate_y, 3)
@@ -39,7 +41,6 @@ class TestInitialization(object):
         eq_(my_point.coordinate_x, 2)
     
     def test_skipping_field_without_default_value(self):
-        Point = Record.create_type("Point", "coordinate_x")
         assert_raises_regexp(
             RecordInitializationError,
             '^Field "coordinate_x" is undefined$',
@@ -47,7 +48,6 @@ class TestInitialization(object):
             )
     
     def test_setting_unknown_field(self):
-        Point = Record.create_type("Point", "coordinate_x", "coordinate_y")
         # By position
         assert_raises_regexp(
             RecordInitializationError,
@@ -69,30 +69,26 @@ class TestInitialization(object):
             )
     
     def test_field_value_set_multiple_times(self):
-        Point = Record.create_type("Point", "coordinate_x")
         assert_raises_regexp(
             RecordInitializationError,
             '^Value of field "coordinate_x" is already set$',
             Point,
             1,
+            3,
             coordinate_x=2,
             )
     
     def test_copy(self):
-        Point = Record.create_type("Point", "coordinate_x", "coordinate_y")
         original_point = Point(1, 3)
         
         derived_point = original_point.copy()
-        eq_(original_point.coordinate_x, derived_point.coordinate_x)
-        
         derived_point.coordinate_y = 5
+        
+        eq_(original_point.coordinate_x, derived_point.coordinate_x)
         eq_(3, original_point.coordinate_y)
         eq_(5, derived_point.coordinate_y)
     
     def test_generalization(self):
-        Point = Record.create_type("Point", "coordinate_x", "coordinate_y")
-        Circle = Point.create_type("Circle", "radius")
-        
         my_circle = Circle(1, 3, 5)
         my_point = Point.init_from_specialization(my_circle)
         ok_(isinstance(my_point, Point))
@@ -100,9 +96,6 @@ class TestInitialization(object):
         eq_(my_point.coordinate_y, my_circle.coordinate_y)
     
     def test_invalid_generalization(self):
-        Point = Record.create_type("Point", "coordinate_x", "coordinate_y")
-        Circle = Point.create_type("Circle", "radius")
-        
         my_point = Point(1, 3)
         assert_raises_regexp(
             RecordInitializationError,
@@ -112,9 +105,6 @@ class TestInitialization(object):
             )
     
     def test_specialization(self):
-        Point = Record.create_type("Point", "coordinate_x", "coordinate_y")
-        Circle = Point.create_type("Circle", "radius")
-        
         my_point = Point(1, 3)
         my_circle = Circle.init_from_generalization(my_point, radius=5)
         ok_(isinstance(my_circle, Circle))
@@ -123,9 +113,6 @@ class TestInitialization(object):
         eq_(my_circle.radius, 5)
     
     def test_invalid_specialization(self):
-        Point = Record.create_type("Point", "coordinate_x", "coordinate_y")
-        Circle = Point.create_type("Circle", "radius")
-        
         my_circle = Circle(1, 3, 5)
         assert_raises_regexp(
             RecordInitializationError,
@@ -135,9 +122,6 @@ class TestInitialization(object):
             )
     
     def test_incomplete_specialization(self):
-        Point = Record.create_type("Point", "coordinate_x", "coordinate_y")
-        Circle = Point.create_type("Circle", "radius")
-        
         my_point = Point(1, 3)
         assert_raises_regexp(
             RecordInitializationError,
@@ -147,9 +131,6 @@ class TestInitialization(object):
             )
     
     def test_specialization_overriding_field_values(self):
-        Point = Record.create_type("Point", "coordinate_x", "coordinate_y")
-        Circle = Point.create_type("Circle", "radius")
-        
         my_point = Point(1, 3)
         assert_raises_regexp(
             RecordInitializationError,
