aae09fde6df8b573129546bcf03db8f51d68a62d
Gustavo Narea <me@gustavonarea.net>
2014-02-23 21:32:41 +0000

    Updated distribution meta-data

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ HERE = path.abspath(path.dirname(__file__))
 VERSION = open(path.join(HERE, "VERSION.txt")).readline().rstrip()
 
 setup(
-    name="PyRecord",
+    name="pyrecord",
     version=VERSION,
     packages=find_packages(),
+    url="https://pypi.python.org/pypi/pyrecord",
+    author="Gustavo Narea",
+    author_email="me@gustavonarea.net",
     )
