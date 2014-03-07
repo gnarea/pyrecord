ddcfd17dcd8c9ecd76e55d2b5b973dcb4623e825
Gustavo Narea <me@gustavonarea.net>
2014-03-07 22:15:34 +0000

    Chose license: Apache License v2

--- pyrecord/_validation/type_validators.py
+++ pyrecord/_validation/type_validators.py
@@ -1,3 +1,17 @@
+# Copyright 2013-2014, Gustavo Narea.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from pyrecord._validation._generic_utils import get_duplicated_iterable_items
 from pyrecord._validation._generic_utils import is_valid_python_identifier
 from pyrecord.exceptions import RecordTypeError
