0a175954a4f85818e14d2198db70bc81d8bda3d9
Gustavo Narea <me@gustavonarea.net>
2014-03-11 23:33:14 +0000

    Updated copyright notice in documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PyRecord'
-copyright = u'2013, Gustavo Narea'
+copyright = u'2013-2014, Gustavo Narea'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
